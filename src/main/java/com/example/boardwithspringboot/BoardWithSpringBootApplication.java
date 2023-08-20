b33eb477079f904b1b39ebbd943daa4bd5968f3f
iamseung <seesungseok@naver.com>
2023-08-20 15:28:05 +0900

    SpringBoot init

---
+++ src/main/java/com/example/boardwithspringboot/BoardWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boardwithspringboot/BoardWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
